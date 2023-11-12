2ff13bc6469f52bc0105afd001c6a71a3f7f5861
Elyas <Elyas.shamal2@gmail.com>
2023-11-12 18:18:44 -0500

    add data from api

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Note() {
+  const [Note, setNote] = useState([]);
+
+  useEffect(() => {
+    let isMouted = true;
+    fetch("https://phase-2-backend-json-server-template.onrender.com/Note")
+      .then((response) => response.json())
+      .then((data) => {
+        if (isMouted) {
+          setNote(data);
+        }
+      });
+
+    return () => {
+      isMouted = false;
+    };
+  }, []);
+
   return (
     <section className="note-section">
-      <div className="note-container"></div>
+      <div className="note-container">
+        {Note.map((notes) => {
+          return (
+            <div className="notes-container" key={notes.id}>
+              <h1>{notes.title}</h1>
+              <p>{notes.content}</p>
+              <button className="delete-btn">X</button>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 }
